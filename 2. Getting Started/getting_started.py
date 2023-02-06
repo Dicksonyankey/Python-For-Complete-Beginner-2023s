fa9b539972e686e8872fc5f218f8b19c7def6723
dicksonyankey <yankeydickson1@gmail.com>
2023-02-06 23:05:19 +0000

    Updated the getting started file

---
+++ 2. Getting Started/getting_started.py
@@ -0,0 +1,69 @@
+# wrote my first program using the print function in python
+print("Hello World")
+
+
+# ------------------------ comments ---------------------
+
+# How to execute a program in python
+
+# step 1 . Read the problem you have at hand 
+print("Hello User")
+
+# step 2 . Come up with name to represent the your variable 
+number = 100
+
+number2 = 200
+# step 3 . Solve the problem by writting in plan english
+
+
+# step 4 . Implementing the coding
+result = number2 + number
+
+print(result)
+
+# step 5. test your program / code
+
+# ------------------------ print function ------------------------
+
+
+print("I love to program using python", end=". ")
+print("Coding in python is fun")
+
+
+print(*"Python", sep="-") # P-y-t-h-o-n
+
+# word = input("Enter a word: ")
+# print(*word, sep=" - ")
+
+# ------------------  Input function in python ---------------------
+
+
+number = int(input("Enter a number: "))
+print(str(number))
+
+print(type(str(number)))
+
+# ---------------------- primitive data types -------------------
+# 1. string 
+# 1. boolean 
+# 1. integer 
+# 1. float 
+
+
+string = "Python"
+print(type(string))
+
+buy = True
+print(type(buy))
+
+sell = False
+print(type(sell))
+
+number = 1000
+print(type(number))
+
+number2 = 100.00
+print(type(number2))
+
+
+
